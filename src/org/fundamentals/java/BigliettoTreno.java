79ba09533db3f9a7a7b2ad9f3a0610e1177f3d69
SebastianBuftea <sebibuftea85@gmail.com>
2024-05-15 14:59:46 +0200

    first exercise completed

--- src/org/fundamentals/java/BigliettoTreno.java
+++ src/org/fundamentals/java/BigliettoTreno.java
@@ -1,2 +1,33 @@
-package org.fundamentals.java;public class BigliettoTreno {
+package org.fundamentals.java;
+
+import java.util.Scanner;
+
+public class BigliettoTreno {
+    public static void main(String[] args) {
+        int age;
+        int distance;
+        double price;
+        double discount;
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Inserire l\'età: ");
+        age = Integer.parseInt(in.nextLine());
+
+        System.out.print("Inserire la distanza in km: ");
+        distance = Integer.parseInt(in.nextLine());
+
+        price= distance * 0.21;
+
+        if(age<18){
+            discount=price*20/100;
+            price=price-discount;
+        }
+        else if (age>=65){
+            discount=price*40/100;
+            price=price-discount;
+        }
+
+        System.out.println((float)price);
+    }
 }
